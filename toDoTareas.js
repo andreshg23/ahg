37ca66a4020e4988a77a9880e35e7882cac36b7b
Andres <anhugar15@gmail.com>
2025-02-12 20:00:05 -0500

    listas

--- toDoTareas.js
+++ toDoTareas.js
@@ -1,11 +1,35 @@
 function agregarTarea(){
     const nuevaTarea = document.getElementById("nuevaTarea");
     const textoTarea = nuevaTarea.value.trim();
+    
+    if(textoTarea === ""){
+        alert("Agregue una tarea");
+        return
+    }
 
     const listadoTarea = document.getElementById("listadoTareas");
 
     const li = document.createElement("li");
-    li.innerHTML = `<span>${textoTarea}</span> <button class="borrar-btn">Eliminar</button>`;
+    li.innerHTML = `<span>${textoTarea}</span> <button class="borrar-btn" onclick="borrarTarea(this)">Eliminar</button>`;
+
+    li.querySelector("span").addEventListener("click", function(){
+        this.parentElement.classList.toggle("completado");
+    });
 
     listadoTarea.appendChild(li);
+
+    nuevaTarea.value = "";
 }
+
+function borrarTarea(elementoTarea){
+    elementoTarea.parentElement.remove();
+
+}
+
+const inputNuevaTarea = document.getElementById("nuevaTarea");
+
+inputNuevaTarea.addEventListener("keypress", function(tecla){
+    if(tecla.key === "Enter"){
+        agregarTarea();
+    }
+});
